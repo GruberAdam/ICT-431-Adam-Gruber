e6adfa5b2e723d7336e67f658ec9ac96947952e9
GruberAdam <48351084+GruberAdam@users.noreply.github.com>
2019-04-03 11:54:32 +0200

    init reposi

---
+++ Code/cmake-build-debug/CMakeFiles/Code.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Code.dir/main.c.obj"
+  "Code.pdb"
+  "Code.exe"
+  "Code.exe.manifest"
+  "libCode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
